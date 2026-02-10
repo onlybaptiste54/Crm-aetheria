b9416b4b9607304dd17e264d63cd0aab72205a06
OnlyBaptiste <157699823+OnlyBaptiste@users.noreply.github.com>
2026-02-10 18:58:47 +0100

    prod

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone', // Pour Docker production
-  // Désactiver Turbopack pour la production
-  experimental: {
-    turbopack: false,
-  },
 };
 
 export default nextConfig;
